876869c1a5bffa6af58307b8228b37a36d8476e8
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-31 15:49:33 +0200

    Bump version to 0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Slug behavior for Meteor Astronomy',
-  version: '0.1.0',
+  version: '0.2.0',
   name: 'jagi:astronomy-slug-behavior',
   git: 'https://github.com/jagi/meteor-astronomy-slug-behavior.git'
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy-behaviors@0.5.1');
+  api.use('jagi:astronomy-behaviors@0.6.0');
   api.use('underscore');
 
   // Behavior.
