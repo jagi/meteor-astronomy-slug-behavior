4d4e590e03c244f8a7165d08c878d549807f969b
jagi <luke.jagodzinski@gmail.com>
2015-07-13 14:05:07 +0200

    Remove console.log

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Slug behavior for Meteor Astronomy',
-  version: '0.3.0',
+  version: '0.3.1',
   name: 'jagi:astronomy-slug-behavior',
   git: 'https://github.com/jagi/meteor-astronomy-slug-behavior.git'
 });
