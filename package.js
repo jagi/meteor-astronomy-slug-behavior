7a8eb084fc9008d04485ead0e85b16c2a3032b94
jagi <luke.jagodzinski@gmail.com>
2015-09-15 18:59:20 +0200

    Introduce initDefinition event

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy-behaviors@0.6.0');
+  api.use('jagi:astronomy@0.12.1');
   api.use('underscore');
 
   // Behavior.
