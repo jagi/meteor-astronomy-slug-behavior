9427334b07ff1f61e82e08734ebd928d91d73ceb
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2016-07-21 10:17:27 +0200

    Bump version to 2.0.1 and lodash dependency

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'jagi:astronomy-slug-behavior',
-  version: '2.0.0',
+  version: '2.0.1',
   summary: 'Slug behavior for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-slug-behavior.git'
 });
 
 Npm.depends({
-  lodash: '4.11.1'
+  lodash: '4.13.1'
 });
 
 Package.onUse(function(api) {
